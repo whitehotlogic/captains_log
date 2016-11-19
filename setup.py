1d105df469eba0d8337868f38e6db3596e2d479a
shane <haroboy876@gmail.com>
2016-11-19 09:47:16 -0800

    add initial setup for setting config file

---
+++ setup.py
@@ -0,0 +1,23 @@
+import json
+from config.config_example import CONFIG as EXAMPLE_CONFIG
+
+NEW_CONFIG = {}
+EXAMPLE_CONFIG = EXAMPLE_CONFIG
+
+
+def get_response(key):
+    response = raw_input("what is the {0}? ".format(key))
+    try:
+        if type(EXAMPLE_CONFIG[key]) == int:
+            response = int(response)
+    except ValueError:
+        print("An integer value must be supplied. Try again: ")
+        return get_response(key)
+    NEW_CONFIG[key] = response
+
+for key in EXAMPLE_CONFIG:
+    get_response(key)
+
+with open("config/config.py", "w") as file:
+    file.write("CONFIG = " + str(json.dumps(
+        NEW_CONFIG, sort_keys=True, indent=4, separators=(',', ': '))))
