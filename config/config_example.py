627881023f07e6a9b5026944b96af979cf56c79a
shane <haroboy876@gmail.com>
2016-11-19 09:46:30 -0800

    add initial example vessel config

---
+++ config/config_example.py
@@ -0,0 +1,13 @@
+CONFIG = {
+  "vessel_name": "VESSEL_NAME",
+  "hull_number": 109876,
+  "uscg_number": 12345,
+  "fuel_capacity": 1,
+  "water_capacity": 2,
+  "battery_capacity": 3,
+  "engine_manufacturer": "ENGINE_MANUFACTURER",
+  "engine_number": 56473,
+  "owner_name": "OWNER_NAME",
+  "owner_certification_agency": "OWNER_CERTIFICATION_AGENCY",
+  "owner_certification_number": "OWNER_CERTIFICATION_NUMBER"
+}
