8844b1788907bd8388072e246eabd0e3517b97ad
shane <haroboy876@gmail.com>
2016-11-21 20:05:28 -0800

    update example config, make into ordered dict

--- config/config_example.py
+++ config/config_example.py
@@ -1,13 +1,17 @@
-CONFIG = {
-  "vessel_name": "VESSEL_NAME",
-  "hull_number": 109876,
-  "uscg_number": 12345,
-  "fuel_capacity": 1,
-  "water_capacity": 2,
-  "battery_capacity": 3,
-  "engine_manufacturer": "ENGINE_MANUFACTURER",
-  "engine_number": 56473,
-  "owner_name": "OWNER_NAME",
-  "owner_certification_agency": "OWNER_CERTIFICATION_AGENCY",
-  "owner_certification_number": "OWNER_CERTIFICATION_NUMBER"
-}
+from collections import OrderedDict
+
+
+CONFIG = OrderedDict([  # maintain order of dict for setup process
+    ("vessel_name", "VESSEL NAME"),
+    ("hull_number", 109876),
+    ("uscg_number", 12345),
+    ("fuel_capacity", 15.0),
+    ("water_capacity", 20.0),
+    ("battery_capacity", 70.0),
+    ("engine_manufacturer", "ENGINE MANUFACTURER"),
+    ("engine_number", 54673),
+    ("engine_type", "diesel"),  # or "gasoline"
+    ("owner_name", "OWNER NAME"),
+    ("owner_certification_agency", "OWNER CERTIFICATION AGENCY"),
+    ("owner_certification_number", "OWNER CERTIFICATION NUMBER")
+])
