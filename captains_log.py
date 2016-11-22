f0df26317a14d5a8449076a7a310bbf5a36f0811
Shane Engelman <haroboy876@gmail.com>
2016-11-21 16:13:18 -0800

    add example scheduled jobs for each hour of the day

--- captains_log.py
+++ captains_log.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
+import schedule
+
+from datetime import datetime
+
 from model.entry import Entry
 
 Entry(db_name="log.db").init_database()
+
+def job(n):
+    print(n)
+
+for hour in range(24):
+    hour = str(hour)
+    if len(hour) == 1:
+        hour = "0" + hour
+    schedule.every().day.at(hour + ":00").do(job, hour)
+
+while True:
+    schedule.run_pending()
 
 # TODO: create vessel entry if not exists
 # TODO: every hour create an entry
