f0730b0e75aa5a0250b367f7b3658377d2900a3f
shane <haroboy876@gmail.com>
2017-04-30 16:34:54 -0700

    fix redirect for urls, cleanup settings

--- captains_log/urls.py
+++ captains_log/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import include, url
 from django.views.generic.base import RedirectView
 from logbook_app import views
-# from rest_framework import routers
 from rest_framework_nested import routers
 
 app_name = "logbook_app"
@@ -15,7 +14,7 @@ router.register(r"portsofcall", views.PortOfCallViewSet)
 router.register(r"notes", views.NoteViewSet)
 
 vessel_day_router = routers.NestedSimpleRouter(
-    router, r"vessels", lookup="vessel")
+    router, r"vessels", lookup="vessels")
 vessel_day_router.register(r"days", views.DayViewSet, base_name="days")
 
 vessel_day_hour_router = routers.NestedSimpleRouter(
@@ -23,10 +22,10 @@ vessel_day_hour_router = routers.NestedSimpleRouter(
 vessel_day_hour_router.register(r"hours", views.HourViewSet, base_name="hours")
 
 urlpatterns = [
-    url(r"^/?$", RedirectView.as_view(url='logbook/api/', permanent=True), name='index'),
-    url(r"^logbook/api/", include(router.urls), name="list"),
-    url(r"^logbook/api/", include(vessel_day_router.urls), name="vessel-day"),
-    url(r"^logbook/api/", include(vessel_day_hour_router.urls), name="vessel-day-hour"),
-    url(r"^logbook/api/vessels/(?P<vessel>[0-9]+)/dates/(?P<date>[0-9]{4}\-[0-9]{2}\-[0-9]{2})/$",
-        views.DateHourViewSet.as_view({"get": "retrieve"}), name="vessel-date"),
+    url(r"^$", RedirectView.as_view(url='logbook/api/')),
+    url(r"^logbook/api/", include(router.urls)),
+    url(r"^logbook/api/", include(vessel_day_router.urls)),
+    url(r"^logbook/api/", include(vessel_day_hour_router.urls)),
+    url(r"^logbook/api/vessels/(?P<vessel>[0-9]+)/dates/(?P<date>[0-9]{4}\-[0-9]{2}\-[0-9]{2})/$",  # noqa
+        views.DateHourViewSet.as_view({"get": "retrieve"})),
 ]
